540a85ee268f769e4d95cb427a62df37d91ff884
Ramanarajan D <ramanarajan.d@msc.com>
2022-08-18 13:19:06 +0530

    Roll the Die! Added

---
+++ ExercismC#/RollTheDie/Player.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+public class Player
+{
+    Random rand = new Random();
+    public int RollDie()
+    {
+        return rand.Next(1, 18);
+    }
+
+    public double GenerateSpellStrength()
+    {
+        return rand.NextDouble() * 100;
+    }
+}
